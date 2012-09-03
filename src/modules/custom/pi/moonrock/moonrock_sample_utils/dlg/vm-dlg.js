7336b54c3c8cd6a19d62b39a3161814f4e2f9353
Eloy Villasclaras <edvf2@linux-iqd4.site>
2012-09-03 17:56:45 +0100

    Revert "Revert "- Fixed sample/snapshot dialog.""
    This reverts commit 50108ae6bebf71e5269d04fab3ea97d65f30925c.

--- src/modules/custom/pi/moonrock/moonrock_sample_utils/dlg/vm-dlg.js
+++ src/modules/custom/pi/moonrock/moonrock_sample_utils/dlg/vm-dlg.js
@@ -5,44 +5,65 @@
 (function($) {
 
   var methods = {
-    init : function(settings) {
-      this.appendTo('body');
-      this.addClass('vm-dlg-dialog');
-      var buttons = $('<div/>').addClass('vm-dlg-buttons').appendTo(this);
-      $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-min').appendTo(buttons).hide().click(function() {
-        $(this).vmDialog('dlg').vmDialog('minimize');
-      });
-      $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-max').appendTo(buttons).click(function() {
-        $(this).vmDialog('dlg').vmDialog('maximize');
-      });
-      $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-close').appendTo(buttons).click(function() {
-        $(this).vmDialog('dlg').vmDialog('close');
-      });
+    init : function(settings, callingElement, callback) {
+      if ($('.vm-dlg-dialog[item-id="' + settings.id + '"]').length === 0) {
+        var self = this;
 
-      $('<div/>').addClass('vm-dlg-content').appendTo(this).load(settings.url, {
-        nid: settings.id,
-        snapshooting: settings.snapshooting
-      });
-      this.data('resize', true).resizable({
-        containment: 'window',
-        start: function(event) {
-          $(this)
-                  .data('resize-width', $(this).width())
-                  .data('resize-height', $(this).height())
-                  .data('resize-x', event.pageX)
-                  .data('resize-y', event.pageY);
-        },
-        resize: function(event) {
-          if ($(this).data('resize')) {
-            $(this).css({
-              width: $(this).data('resize-width') + event.pageX - $(this).data('resize-x'),
-              height: $(this).data('resize-height') + event.pageY - $(this).data('resize-y')
-            });
+        this.appendTo('body').addClass('vm-dlg-dialog').attr('item-id', settings.id);
+
+
+        var buttons = $('<div/>').addClass('vm-dlg-buttons').appendTo(this);
+        $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-min').appendTo(buttons).hide().click(function() {
+          $(this).vmDialog('dlg').vmDialog('minimize');
+        });
+        $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-max').appendTo(buttons).click(function() {
+          $(this).vmDialog('dlg').vmDialog('maximize');
+        });
+        $('<div/>').addClass('vm-dlg-icon vm-dlg-icon-close').appendTo(buttons).click(function() {
+          $(this).vmDialog('dlg').vmDialog('close');
+        });
+        $('<div/>').addClass('vm-dlg-title').appendTo(buttons);
+
+        $('<div/>').addClass('vm-dlg-content').appendTo(this).load(settings.url, {
+          nid: settings.id,
+          snapshooting: settings.snapshooting
+        }, function() {
+          callback(self);
+        });
+
+        this.data('resize', true).resizable({
+          containment: 'window',
+          start: function(event) {
+            $(this)
+                    .data('resize-width', $(this).width())
+                    .data('resize-height', $(this).height())
+                    .data('resize-x', event.pageX)
+                    .data('resize-y', event.pageY);
+          },
+          resize: function(event) {
+            if ($(this).data('resize')) {
+              $(this).css({
+                width: $(this).data('resize-width') + event.pageX - $(this).data('resize-x'),
+                height: $(this).data('resize-height') + event.pageY - $(this).data('resize-y')
+              });
+            }
           }
-        }
-      });
-      this.draggable({containment: 'window'});
-      return this;
+        });
+        this.draggable({containment: 'window'});
+
+        this.css({
+          top:  $(callingElement).offset().top - $(window).scrollTop(),
+          left:  $(callingElement).offset().left - $(window).scrollLeft(),
+          width:  $(callingElement).width(),
+          height:  $(callingElement).height()
+        });
+      } else {
+        self = $('.vm-dlg-dialog[item-id="' + settings.id + '"]');
+      }
+
+      self.vmDialog('maximize');
+
+      return self;
     },
     dlg: function() {
       return this.parents('.vm-dlg-dialog');
@@ -65,13 +86,13 @@
         left: 0,
         width: $(window).width() - 20,
         height: $(window).height() - 20
-      }, 'fast', function() {
+      }, 'slow', function() {
         $(this).data('resize', false).find('.vm-dlg-icon-min').show().end()
                 .find('.vm-dlg-icon-max').hide();
       });
     },
     minimize: function() {
-      this.animate(this.data('restore'), 'fast', function() {
+      this.animate(this.data('restore'), 'slow', function() {
         $(this).data('resize', true).find('.vm-dlg-icon-min').hide().end()
                 .find('.vm-dlg-icon-max').show();
       });
